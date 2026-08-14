dd03d74bfdd88507ea5698258f2a9bb822283ef7
Mario Alba <iscmap@gmail.com>
2026-08-14 12:25:37 -0600

    step-8.1-service-dockerfiles

--- availability-service/src/main/java/com/barbersaas/availability/adapters/out/deduplication/dynamodb/DynamoDbConfiguration.java
+++ availability-service/src/main/java/com/barbersaas/availability/adapters/out/deduplication/dynamodb/DynamoDbConfiguration.java
@@ -1,6 +1,7 @@
 package com.barbersaas.availability.adapters.out.deduplication.dynamodb;
 
 import java.net.URI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -12,12 +13,13 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 public class DynamoDbConfiguration {
 
   @Bean
-  public DynamoDbClient dynamoDbClient() {
+  public DynamoDbClient dynamoDbClient(
+          @Value("${spring.cloud.aws.endpoint:http://localhost:4566}") String awsEndpoint) {
     return DynamoDbClient.builder()
-        .endpointOverride(URI.create("http://localhost:4566"))
-        .region(Region.US_EAST_1)
-        .credentialsProvider(
-            StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")))
-        .build();
+            .endpointOverride(URI.create(awsEndpoint))
+            .region(Region.US_EAST_1)
+            .credentialsProvider(
+                    StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")))
+            .build();
   }
 }
--- booking-service/src/main/java/com/barbersaas/booking/adapters/out/persistence/idempotency/DynamoDbConfiguration.java
+++ booking-service/src/main/java/com/barbersaas/booking/adapters/out/persistence/idempotency/DynamoDbConfiguration.java
@@ -1,6 +1,7 @@
 package com.barbersaas.booking.adapters.out.persistence.idempotency;
 
 import java.net.URI;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -10,13 +11,17 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @Configuration
 public class DynamoDbConfiguration {
+
   @Bean
-  public DynamoDbClient dynamoDbClient() {
+  public DynamoDbClient dynamoDbClient(
+          @Value("${spring.cloud.aws.endpoint:http://localhost:4566}") String awsEndpoint) {
+
     return DynamoDbClient.builder()
-        .endpointOverride(URI.create("http://localhost:4566"))
-        .region(Region.US_EAST_1)
-        .credentialsProvider(
-            StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")))
-        .build();
+            .endpointOverride(URI.create(awsEndpoint))
+            .region(Region.US_EAST_1)
+            .credentialsProvider(
+                    StaticCredentialsProvider.create(
+                            AwsBasicCredentials.create("test", "test")))
+            .build();
   }
 }
